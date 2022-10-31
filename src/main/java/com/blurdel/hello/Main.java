9e773cde5351d3d21a752125ed3eb8976a9b2df7
blurdel <5648737+blurdel@users.noreply.github.com>
2022-10-31 17:30:34 -0400

    Added manifest file to pom

--- src/main/java/com/blurdel/hello/Main.java
+++ src/main/java/com/blurdel/hello/Main.java
@@ -1,12 +1,12 @@
-package com.blurdel;
+package com.blurdel.hello;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class HelloWorld {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(StringUtils.capitalize("Hello World!"));
+        // System.out.println(StringUtils.capitalize("Hello World!"));
     }
 
 }
